a31fb80d2698a9b28cb9050c88a3cb7ea2d00d11
John Huffman <jmhuffman37@gmail.com>
2020-07-14 15:00:39 -0500

    finished to do list

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+      
+        public bool IsAPalindrome(string word)
+        {
+            // using a for loop to solve
+            var reversed = "";
+
+            for (int i = word.Length -1 ; i >= 0; i--)
+            {
+                reversed += word[i]; // reversed = reversed + word
+            };
+
+            if (reversed == word)
+                return true;
+            else
+                return false;
+
+
+        }
+    }
+}
